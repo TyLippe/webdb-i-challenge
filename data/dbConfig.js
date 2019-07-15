c40030aae7e541933ec82d5ecb29879b7bb666dc
Ty Lippe <tylippe94@gmail.com>
2019-07-15 18:34:57 -0400

    endpoints running!

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -11,30 +11,78 @@ const dbConnector = knex({
 
 const router = express.Router();
 
+//GET ALL (WORKING)
 router.get('/', (req, res) => {
-    dbConnector('budget')
-    .then(budget => {
-        res.status(200).json(budget);
+    dbConnector('accounts')
+    .then(account => {
+        res.status(200).json(account);
     })
     .catch(err => {
         res.status(500).json(err)
     })
 });
 
+//GET BY ID(WORKING)
 router.get('/:id', (req, res) => {
-
+    dbConnector('accounts')
+        .where({ id: req.params.id })
+        .first()
+        .then(account => {
+            if (account) {
+                res.status(200).json(account);
+            } else {
+                res.status(400).json({ message: 'not found'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
 })
 
+//ADD AN ACCOUNT (WORKING)
 router.post('/', (req, res) => {
+    const account = req.body;
 
+    dbConnector('accounts')
+        .insert(account, 'id')
+        .then(arrayOfIds => {
+            const idOfLast = arrayOfIds[0];
+
+            res.status(201).json(idOfLast);
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
 })
 
+//UPDATE AN ACCOUNT (WORKING)
 router.put('/:id', (req, res) => {
-
+    dbConnector('accounts')
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: `${count} record(s) updated` })
+            } else {
+                res.status(404).json({ message: 'not found' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
 })
 
+//DELETE ACCOUNT (WORKING)
 router.delete('/:id', (req, res) => {
-
+    dbConnector('accounts')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            res.status(200).json({ message: `${count} record(s) deleted.` })
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
 })
 
 module.exports = router;
